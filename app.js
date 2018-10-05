b10229472decd55998ff2854a932600f48e973c0
marcosfp <contato@marcos.biz>
2018-10-05 10:59:49 -0300

    Last updated version from section

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-let command = process.argv[2];
+let command = argv._[0];
 
 console.log('Command:', command);
 console.log('Yargs:', argv);
